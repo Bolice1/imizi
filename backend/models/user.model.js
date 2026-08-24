21836647df018b5846881df2aa5c0e325ab3ce13
Bolice1 <komezusengebolice@gmail.com>
2026-08-24 16:57:40 +0200

    feat: Added user and Family models

---
+++ backend/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+export const userSchema = new mongoose.Schema({
+   fullName:{
+        type: String,
+        required: true,
+        unique: false
+   },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        unique: false
+    },
+    familyId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Family',
+        required: false,
+        unique: false
+    },
+    role:{
+        type: String,
+        unique: false,
+        enum: ['admin_family', 'user'],
+        default: 'user'
+    },
+    phoneNumber:{
+        type: String,
+        required: false,
+        unique: true,
+    }
+})
+
+
+export const  UserModel = mongoose.model('User', userSchema);
